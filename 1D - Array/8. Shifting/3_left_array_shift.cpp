6f702ddb1b11890d8b1fe96b52b3802b2b8a672f
Aman Gupta <amangupta6121998@gmail.com>
2020-09-28 15:49:13 +0530

    Add files via upload

---
+++ 1D - Array/8. Shifting/3_left_array_shift.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n,i,a[100000],pos;
+
+
+  cin>>n;
+
+  for( i=0;i<n;i++)
+  {
+    cin>>a[i];
+  }
+
+  cout<<"\nEnter the position of shifting\n";
+  cin>>pos;
+
+      if(pos>=n)
+      {
+          pos=pos-n;
+      }
+
+      for( i=0;i<n;i++)
+      {
+            a[i]=a[i+pos];
+      }
+
+
+
+    // displaying reversed Array
+
+    for( i=0;i<n;i++)
+    {
+      cout<<a[i]<<" ";
+    }
+
+
+
+return 0;
+  }
